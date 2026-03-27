b26c2b922121097eea9613127c8179e3f6d75064
Angel Sanchez <jnrsanchezguzman@gmail.com>
2026-03-27 07:40:11 -0400

    Agregar archivos de proyecto.

---
+++ AplicacionWebDePrueba_(no_se_me_ocurre_otro_nombre)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AplicacionWebDePrueba__no_se_me_ocurre_otro_nombre_.MvcApplication" Language="C#" %>
---
+++ AplicacionWebDePrueba_(no_se_me_ocurre_otro_nombre)/Global.asax.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AplicacionWebDePrueba__no_se_me_ocurre_otro_nombre_
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
